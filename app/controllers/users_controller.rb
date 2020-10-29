28b9cfb0163750de5855460e2b743e37b9214f0d
jean <macyjeanma@gmail.com>
2020-10-29 14:49:36 +0800

    create user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+  def sign_in
+    @user = User.new
+  end
+
+  def login
+    if user_params[:account] && user_params[:password]
+      user = User.login(user_params)
+      if user
+        sign_in_user(user)
+      else
+        redirect_to sign_in_users_path, notice: "請輸入正確帳號密碼"
+      end
+    else
+      redirect_to sign_in_users_path, notice: "請輸入正確帳號密碼"
+    end
+  end
+
+  def sign_up
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # 登入
+      sign_in_user(@user)
+      redirect_to root_path, notice: '會員註冊成功'
+    else
+      render :sign_up
+    end
+  end
+
+  def sign_out
+    sign_out_user
+    redirect_to root_path, notice: '登出成功'
+  end
+
+  private
+  def sign_in_user(u)
+    session[:user_token] = u.id
+  end
+
+  def sign_out_user
+    session[:user_token] = nil
+  end
+
+  def user_params
+    params.require(:user).permit(:account, :password, :email)
+  end
+end
--- app/models/free_cupon.rb
+++ app/models/free_cupon.rb
@@ -1,2 +1,14 @@
 class FreeCupon < ApplicationRecord
+  validates :compony, presence: true, uniqueness: true
+  validates :serial_number, presence: true, uniqueness: true
+  belongs_to :user
+  has_many :comments
+  
+  def display_username
+    if user.nil?
+      "未知"
+    else
+      user.account
+    end
+  end
 end
