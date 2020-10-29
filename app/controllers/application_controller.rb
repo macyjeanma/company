1806a704968118449ec105852dfa0d13e0257ff4
jean <macyjeanma@gmail.com>
2020-10-29 21:11:25 +0800

    fix navbar $ footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :user_signed_in?, :current_user
+
+  private
+  
+  def user_signed_in?
+    current_user != nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_token])
+  end
+
+  def authenticate_user!
+    redirect_to root_path, notice: "請登入會員" if not user_signed_in?
+  end
 end
--- app/controllers/free_cupons_controller.rb
+++ app/controllers/free_cupons_controller.rb
@@ -1,5 +1,6 @@
 class FreeCuponsController < ApplicationController
-  before_action :find_user only: [:show, :edit, :update, :destroy]  
+  before_action :find_user only: [:show, :edit, :update, :destroy] 
+  before_action :authenticate_user!
   def index
     @free_cupon = free_cupon.all
   end
